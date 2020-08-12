9dec400b12321ed773304144f38e6d391e8c62a8
Eder Montano <eder.montano@astrazeneca.com>
2020-08-12 18:17:19 -0500

    Tarea cascaron Left & Main

---
+++ Equipo1_bootcamp/src/test/java/MainArea/FunctionalTestCases.java
@@ -0,0 +1,37 @@
+package MainArea;
+
+
+import org.testng.annotations.Test;
+
+public class FunctionalTestCases {
+
+    @Test(description = "Este test case verifica que las categorias esperadas se muestren en el main area usando un usuario no logueado")
+    public static void VideoCategories{
+
+    }
+
+    @Test(description = "Este test case verifica que los componentes del video se muestren en el main area usando un usuario no logueado")
+    public  static void VideoComponents{
+
+    }
+
+    @Test(description = "Este test case verifica que el usuario no logueado puede reproducir un video")
+    public  static void ReproduccionDelVideo{
+
+    }
+
+    @Test(description = "Este test case verifica que el usuario no logueado ver los detalles del video")
+    public  static void VideoDetails{
+
+    }
+
+    @Test(description = "Este test case verifica que el usuario no logueado puede buscar un video")
+    public  static void VideoSearch{
+
+    }
+
+    @Test(description = "Este test case verifica que el usuario no logueado pueda añadir un video a una playlist")
+    public  static void AddVideoToAPlaylist{
+
+    }
+}
