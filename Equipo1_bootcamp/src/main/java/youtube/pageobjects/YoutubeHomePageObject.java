e2dbbb882cfad1e34c5f6a50ec6522741f64e4c4
Maricela Romero <maryxelaromer@gmail>
2020-08-27 18:41:48 -0500

    Update the YoutubeHomePageObaject

--- Equipo1_bootcamp/src/main/java/youtube/pageobjects/YoutubeHomePageObject.java
+++ Equipo1_bootcamp/src/main/java/youtube/pageobjects/YoutubeHomePageObject.java
@@ -1,6 +1,5 @@
 package youtube.pageobjects;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -12,11 +11,15 @@ import java.util.List;
 public class YoutubeHomePageObject extends BasePageObject {
 
     //ELEMENTOS DEL LEFT MENU - PRINCIPAL, TENDECIAS, BIBLIOTECA E HISTORIAL
-    @FindBy(how = How.XPATH, using = "//body//ytd-guide-section-renderer//*[a] [position()<=2]")
+    @FindBy(how = How.XPATH, using = "*//a[@class='yt-simple-endpoint style-scope ytd-guide-entry-renderer']")
     private List<WebElement> resultsOptionsLeftMenu;
 
-    @FindBy(how = How.XPATH, using = "//ytd-mini-guide-entry-renderer[1]//a[1]")
+    @FindBy(how = How.XPATH, using = "//div[@id='sections']/ytd-guide-section-renderer[1]/div[1]/ytd-guide-entry-renderer[1]/a[1]")
     private WebElement homeOption;
+
+    @FindBy (how = How.XPATH,using = "//div[@id='contents']//div[@id='rich-shelf-header']//span")
+    //private WebElement titleTrending;
+    private List<WebElement> titlesInMain;
 
     @FindBy(how = How.XPATH, using = "//ytd-mini-guide-entry-renderer[2]//a[1]")
     private WebElement trendingOption;
@@ -54,7 +57,7 @@ public class YoutubeHomePageObject extends BasePageObject {
     @FindBy(how = How.XPATH, using = "")
     private WebElement subscriptionsMessage;
 
-    @FindBy(how = How.XPATH, using = "//ytd-mini-guide-entry-renderer[4]//a[1]")
+    @FindBy(how = How.XPATH, using = "//ytd-guide-entry-renderer[@id='header-entry']//a[@id='endpoint']")
     private WebElement libraryOption;
 
     @FindBy(how = How.XPATH, using = "")
@@ -71,22 +74,51 @@ public class YoutubeHomePageObject extends BasePageObject {
 
     // AQUI VAN LOS ELEMENTOS DEL MAIN AREA EN HOME.
 
-    public YoutubeHomePageObject(WebDriver driver, String baseURL) {
-        super(driver, baseURL);
+    public YoutubeHomePageObject(WebDriver driver) {
+        super(driver, driver.getCurrentUrl());
     }
 
-    public void getOptionsLeftMenu() {
-        int i = 0;
-        while (i < resultsOptionsLeftMenu.size()) {
-            System.out.println(resultsOptionsLeftMenu.get(i).getText());
-            i++;
+    public int getOptionsLeftMenuList() {
+        ArrayList<String> results = new ArrayList<>();
+
+        for (WebElement element : resultsOptionsLeftMenu) {
+            System.out.println("Opciones del menu izquierdo:"+ element.getText());
+            results.add(element.getText());
         }
+        int x = 0;
+        int totalCategories = 0;
+        while (x < results.size()) {
+            if (results.get(x).contains("Principal") && results.get(x).contains("Tendencias") && results.get(x).contains("Suscripciones") && results.get(x).contains("Biblioteca") && results.get(x).contains("Historial")) {
+                totalCategories++;
+            }
+            x++;
+        }
+        return totalCategories;
     }
-
     public void redirectsMainPage(){
         if (this.homeOption.isDisplayed() && this.homeOption.isEnabled())
-        { this.homeOption.click();}
+        { this.homeOption.click(); }
     }
+
+    public int TrendingTitleIsDisplayed(){
+
+        ArrayList<String> results= new ArrayList<>();
+        for (WebElement element : titlesInMain)
+        {
+            System.out.println("Secciones en dentro del Main:"+ element.getText());
+            results.add(element.getText());
+        }
+        int x=0;
+        int totalTitles=0;
+        while(x<results.size()){
+            if(results.get(x).contains("Tendencias") && results.get(x).contains("Noticias sobre el COVID‑19")){
+                totalTitles++;
+            }
+            x++;
+        }
+        return totalTitles;
+    }
+
 
     public void redirectsTrendingCategory() throws InterruptedException {
         if(this.trendingOption.isDisplayed() && this.trendingOption.isEnabled()){
@@ -144,8 +176,8 @@ public class YoutubeHomePageObject extends BasePageObject {
     public void redirectsSubscriptionPage() throws InterruptedException {
         if (this.subscriptionsOption.isDisplayed() && this.subscriptionsOption.isEnabled())
         { this.subscriptionsOption.click();
-          Thread.sleep(3000);
-          this.subscriptionsMessage.getText();
+            Thread.sleep(3000);
+            this.subscriptionsMessage.getText();
         }
     }
 
@@ -153,15 +185,15 @@ public class YoutubeHomePageObject extends BasePageObject {
         if (this.libraryOption.isDisplayed() && this.libraryOption.isEnabled())
         { this.libraryOption.click();
             Thread.sleep(3000);
-          this.libraryMessage.getText();
+            this.libraryMessage.getText();
         }
     }
 
     public void redirectsHistoryPage() throws InterruptedException {
         if (this.historyOption.isDisplayed() && this.historyOption.isEnabled())
         { this.historyOption.click();
-          Thread.sleep(3000);
-          this.historyMessage.getText();
+            Thread.sleep(3000);
+            this.historyMessage.getText();
         }
     }
 }
